e5e3cc4def57590ce9493266a4a7eafb02082d29
the-one-who-knoccks <thiagoalves.devp@gmail.com>
2021-08-09 20:19:31 -0300

    :lipstick: Created global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+
+
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+  -webkit-font-smoothing: antialiased;
+}
+
+h1, h2, h3, h4, p {
+  font-family: 'Poppins', sans-serif;
+  color: #ffffff;
+}
+
+
+`;
